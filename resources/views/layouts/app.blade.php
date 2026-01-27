c6d77c041259fb818f7b25418f6467c95b7a684b
nurmieai <nurmieai@gmail.com>
2026-01-27 13:34:09 +0700

    setspawn, no error, plus (+) sidebar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,15 +2,51 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Laravel Livewire</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite('resources/css/app.css')
+    <title>{{ $title ?? config('app.name', 'Laravel App') }}</title>
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
-<body class="bg-gray-100">
 
-    {{ $slot }}
+<body class="bg-gray-100 min-h-screen">
 
-    @livewireScripts
+<div class="drawer lg:drawer-open">
+
+    {{-- Toggle Mobile --}}
+    <input id="app-drawer" type="checkbox" class="drawer-toggle" />
+
+    {{-- ================= CONTENT ================= --}}
+    <div class="drawer-content flex flex-col">
+
+        {{-- Navbar --}}
+        @isset($navbar)
+            {{ $navbar }}
+        @else
+            <x-navbar />
+        @endisset
+
+        {{-- Page Content --}}
+        <main class="flex-1 p-4 md:p-6">
+            {{ $slot }}
+        </main>
+
+    </div>
+
+    {{-- ================= SIDEBAR ================= --}}
+    <div class="drawer-side">
+        <label for="app-drawer" class="drawer-overlay"></label>
+
+        @isset($sidebar)
+            {{ $sidebar }}
+        @else
+            <x-sidebar />
+        @endisset
+    </div>
+
+</div>
+
+@livewireScripts
 </body>
 </html>
