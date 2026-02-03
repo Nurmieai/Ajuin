e5a422d41724c70f9c13bc3499260556be5fa604
Nurmieai <nurmieai@gmail.com>
2026-02-03 13:12:07 +0700

    main

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,4 @@ use Livewire\Volt\Volt;
 
 
 Route::get('/login', \App\Livewire\Auth\Login::class)->name('login');
-Route::get('/rgister', \App\Livewire\Auth\Register::class)->name('register');
+Route::get('/register', \App\Livewire\Auth\Register::class)->name('register');
