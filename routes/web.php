c2a8e149d29bd59ba87656be29f90f6dba47bbc4
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2026-02-04 12:59:31 +0700

    Penambahan route dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 
 Route::get('/login', \App\Livewire\Auth\Login::class)->name('login');
 Route::get('/register', \App\Livewire\Auth\Register::class)->name('register');
+
+use App\Livewire\Dashboard;
+
+Route::get('/dashboard', Dashboard::class);
