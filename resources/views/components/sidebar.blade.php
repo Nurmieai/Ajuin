ce88ad2a467c7081739361fd5e1ca41c82e769c4
Nurmieai <nurmieai@gmail.com>
2026-02-09 21:44:30 +0700

    dashboard hasrole and some fix

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -37,7 +37,6 @@
                 </form>
             </div>
 
-            @hasrole('teacher')
             <div class="flex flex-col h-max justify-center gap-2">
                 <li>
                     <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Bank PKL">
@@ -51,7 +50,7 @@
                         </span>
                     </button>
                 </li>
-
+                @hasrole('teacher')
                 <li>
                     <a href="{{ route('partners.index') }}" class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Mitra PKL">
 
@@ -89,6 +88,22 @@
                         </span>
                     </button>
                 </li>
+
+                <form action="{{ route('activation') }}">
+                    <li>
+                        <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Aktivasi siswa">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                            </svg>
+
+
+                            <span class="is-drawer-close:hidden">
+                                Aktivasi Siswa
+                            </span>
+                        </button>
+                    </li>
+                </form>
                 @endhasrole
 
                 @hasrole('student')
@@ -105,38 +120,8 @@
                         </span>
                     </button>
                 </li>
-                @hasrole('teacher')
-                <form action="{{ route('activation') }}">
-                    <li>
-                        <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Aktivasi siswa">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                            </svg>
-
-
-                            <span class="is-drawer-close:hidden">
-                                Aktivasi Siswa
-                            </span>
-                        </button>
-                    </li>
-                </form>
                 @endhasrole()
             </div>
-                <li>
-                    <button class="is-drawer-close:tooltip is-drawer-close:tooltip-right" data-tip="Aktivasi siswa  ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
-                        </svg>
-
-
-                        <span class="is-drawer-close:hidden">
-                            Aktivasi Siswa
-                        </span>
-                    </button>
-                </li>
-            @endhasrole
         </ul>
     </div>
 </div>
