ba96a4bdecaf017404268e120b24ae1f7a0aa203
Muhamad Gia Mardhotillah <gia95194@gmail.com>
2026-02-19 10:29:19 +0700

    Menguvbah tampilan input

--- resources/views/components/ui/input.blade.php
+++ resources/views/components/ui/input.blade.php
@@ -1,4 +1,9 @@
-@props(['name', 'label' => null, 'type' => 'text', 'placeholder' => ''])
+@props([
+'name',
+'label' => null,
+'type' => 'text',
+'placeholder' => ''
+])
 
 <div class="w-full">
     @if($label)
@@ -16,8 +21,14 @@
     text-slate-800 dark:text-slate-100
     placeholder-slate-400 dark:placeholder-slate-500
     border
-    focus:ring-1 dark:fokus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-500
+    focus:ring-1 dark:focus:ring-2 focus:ring-blue-300 dark:focus:ring-blue-500
     focus:border-blue-600 dark:focus:border-blue-500
+    file:mr-4 file:py-2 file:px-4
+    file:rounded-l-md file:border-0
+    file:text-sm file:font-semibold
+    file:bg-blue-50 file:text-blue-700
+    hover:file:bg-blue-100 dark:hover:file:bg-blue-500
+    dark:file:bg-slate-800 dark:file:text-slate-200
     ";
 
     $borderClass = $errors->has($name)
@@ -25,13 +36,30 @@
     : 'border-slate-300 dark:border-slate-700';
     @endphp
 
+    {{-- TEXTAREA --}}
     @if($type === 'textarea')
     <textarea
         id="{{ $name }}"
         wire:model="{{ $name }}"
         placeholder="{{ $placeholder }}"
         {{ $attributes->merge(['class' => "textarea textarea-bordered h-24 $baseClass $borderClass"]) }}>
-    </textarea>
+        </textarea>
+
+    @elseif($type === 'file')
+    <fieldset class="fieldset">
+        <legend class="fieldset-legend">{{ $label ?? 'Upload File' }}</legend>
+
+        <input
+            id="{{ $name }}"
+            type="file"
+            wire:model="{{ $name }}"
+            {{ $attributes->merge(['class' => "file-input file-input-bordered w-full $baseClass $borderClass"]) }} />
+
+        <label class="label">
+            <span class="label-text-alt">Max size 2MB</span>
+        </label>
+    </fieldset>
+
     @else
     <input
         id="{{ $name }}"
