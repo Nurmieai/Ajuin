63a80e0e9cbbb860737e4f6f2c1b5ee5514919c8
Nurmieai <nurmieai@gmail.com>
2026-04-22 20:08:10 +0700

    Minor Changes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,7 +20,7 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $admin = User::create([
-            'username'  => 'admin',
+            'username'  => 'Guru',
             'email'     => 'admin@gmail.com',
             'fullname'  => 'Admin Ganteng',
             'password'  => bcrypt('admin123'),
@@ -29,17 +29,53 @@ class DatabaseSeeder extends Seeder
 
         $admin->assignRole('teacher');
 
-        $student = User::create([
-            'username'  => 'Yuk',
-            'email'     => 'yuk@gmail.com',
-            'fullname'  => 'Yuktafi',
-            'nisn'      => '89098098029',
-            'password'  => bcrypt('abcde123'),
-            'is_active' => true,
-            'major_id'  => 1,
-        ]);
 
-        $student->assignRole('student');
+        // 5 akun siswa
+        $students = [
+            [
+                'username' => 'Yuk',
+                'email' => 'yuk@gmail.com',
+                'fullname' => 'Yuktafi',
+                'nisn' => '89098098029',
+            ],
+            [
+                'username' => 'Nabil',
+                'email' => 'nurmieai@gmail.com',
+                'fullname' => 'Nabil Nur Rahmat',
+                'nisn' => '89098098030',
+            ],
+            [
+                'username' => 'Aldo',
+                'email' => 'aldo@gmail.com',
+                'fullname' => 'Aldo Yonanda Firmansyah',
+                'nisn' => '89098098031',
+            ],
+            [
+                'username' => 'Afif',
+                'email' => 'afif@gmail.com',
+                'fullname' => 'Afif Rayhan Habibi',
+                'nisn' => '89098098032',
+            ],
+            [
+                'username' => 'Gia',
+                'email' => 'gia@gmail.com',
+                'fullname' => 'Muhammad Gia Mardhotillah',
+                'nisn' => '89098098033',
+            ],
+        ];
+
+        foreach ($students as $data) {
+            $student = User::create([
+                ...$data,
+                'password'  => bcrypt('abcde123'),
+                'is_active' => true,
+                'major_id'  => 1,
+            ]);
+
+            $student->assignRole('student');
+        }
+
+
         $role = Role::firstOrCreate(['name' => 'student']);
 
         User::factory()
@@ -51,6 +87,7 @@ class DatabaseSeeder extends Seeder
             ->each(function ($user) use ($role) {
                 $user->assignRole($role);
             });
+
         Submission::factory()->count(10)->create();
     }
 }
